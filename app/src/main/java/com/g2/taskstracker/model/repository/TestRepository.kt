14d922e8235b064d356fd3967d8bac9cf856039a
Pham Tuan <phamtuan199911@gmail.com>
2020-05-07 15:33:38 +0700

    change architect to fit with firebase

---
+++ app/src/main/java/com/g2/taskstracker/model/repository/TestRepository.kt
@@ -0,0 +1,49 @@
+package com.g2.taskstracker.model.repository
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.*
+
+class TestRepository {
+    companion object {
+        private const val TAG = "KotlinActivity"
+    }
+
+    private val data: MutableLiveData<String> = MutableLiveData()
+
+    fun basicReadWrite(): LiveData<String> {
+        // [START write_message]
+        // Write a message to the database
+        val database = Firebase.database
+        Log.d(TAG, "Db is: $database")
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!" + UUID.randomUUID())
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                data.value = dataSnapshot.value as String
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+        // [END read_message]
+
+        return data
+    }
+
+}
