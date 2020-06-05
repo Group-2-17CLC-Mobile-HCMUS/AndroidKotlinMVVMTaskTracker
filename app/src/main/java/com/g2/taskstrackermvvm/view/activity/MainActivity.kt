bfb8c2dfa4cebd023d1ed1596551642176592801
Pham Tuan <phamtuan199911@gmail.com>
2020-06-05 14:30:02 +0700

    dark theme

--- app/src/main/java/com/g2/taskstrackermvvm/view/activity/MainActivity.kt
+++ app/src/main/java/com/g2/taskstrackermvvm/view/activity/MainActivity.kt
@@ -2,42 +2,46 @@ package com.g2.taskstrackermvvm.view.activity
 
 import android.content.Context
 import android.content.Intent
-import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.util.Log
 import android.view.MenuItem
-import android.view.View
-import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.get
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.firebase.ui.auth.AuthUI
 import com.g2.taskstrackermvvm.R
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.nav_header.view.*
-import kotlinx.android.synthetic.main.switch_theme.*
 
 
 class MainActivity : AppCompatActivity() {
-    private var darkTheme =  false;
+    private var darkTheme = false;
+
     companion object {
         fun startActivity(context: Context) {
             val intent = Intent(context, MainActivity::class.java)
             context.startActivity(intent)
         }
+
+        const val DARK_THEME_BUNDLE_KEY = "com.g2.taskstrackermvvm.view.activity.MainActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        savedInstanceState?.let {
+            darkTheme = it.getBoolean(DARK_THEME_BUNDLE_KEY)
+        }
 
         val navController = findNavController(R.id.frag_container)
         val appBarConfig = AppBarConfiguration(
@@ -52,35 +56,45 @@ class MainActivity : AppCompatActivity() {
         )
 
 
-//            if (!switch.isChecked) {
-//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-//            } else {
-//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-//            }
-
-
-
         toolbar.setupWithNavController(navController, appBarConfig)
         nav_view.setupWithNavController(navController)
 //        nav_view.menu.findItem(R.id.switch_theme_item).apply {
 //            setOnMenuItemClickListener { changeTheme() }
 //        }
-        nav_view.menu.findItem(R.id.sign_out_item).apply {
-            val s = SpannableString(title)
-            s.setSpan(ForegroundColorSpan(Color.RED), 0, s.length, 0)
-            title = s
+        nav_view.menu.apply {
+            findItem(R.id.sign_out_item).apply {
+                val s = SpannableString(title)
+                s.setSpan(ForegroundColorSpan(Color.RED), 0, s.length, 0)
+                title = s
 
-            setOnMenuItemClickListener {
-                signOut()
+                setOnMenuItemClickListener {
+                    signOut()
+                }
             }
+
+            findItem(R.id.switch_theme_item).apply {
+                val switch = this.actionView.findViewById<SwitchMaterial>(R.id.switch_theme)
+                Log.d(
+                    "com.g2.taskstrackermvvm.view.activity.MainActivity",
+                    darkTheme.toString()
+                )
+                switch?.isChecked = darkTheme
+                switch?.setOnCheckedChangeListener { _, isChecked ->
+                    darkTheme = isChecked
+                    if (darkTheme)
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+            }
+
         }
+
 
         val user = FirebaseAuth.getInstance().currentUser
         user?.let {
             val navHeader = nav_view.getHeaderView(0)
             navHeader.nav_header_email.text = it.email
             navHeader.nav_header_display_name.text = it.displayName
-
         }
 
 
@@ -96,28 +110,15 @@ class MainActivity : AppCompatActivity() {
 //        }
     }
 
-    
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.switch_theme_item)
-        {
-            val switch = this.findViewById<Switch>(R.id.switch_theme)
-            darkTheme = !darkTheme
-            print(darkTheme.toString())
-            switch.isChecked = darkTheme
-            switch?.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked)
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
-            }
-        }
-
-        val navController = findNavController(R.id.nav_host_fragment_container)
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
-
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(DARK_THEME_BUNDLE_KEY, darkTheme)
+        super.onSaveInstanceState(outState)
     }
 
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
 
     private fun signOut(): Boolean {
         AuthUI.getInstance().signOut(this)
@@ -130,7 +131,8 @@ class MainActivity : AppCompatActivity() {
             }
         return true
     }
-//    private fun ChangeTheme(): Boolean {
+
+    //    private fun ChangeTheme(): Boolean {
 //        val switch = this.findViewById<Switch>(R.id.switch_theme)
 //        darkTheme = !switch.isChecked
 //        switch.isChecked = darkTheme
