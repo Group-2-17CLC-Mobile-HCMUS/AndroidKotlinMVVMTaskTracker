142139f0a3332c2fca157640145d941b47fccabb
Cao Dinh Vi <caodvi@gmail.com>
2020-06-11 01:45:45 +0700

    Add calendar's function

--- app/src/main/java/com/g2/taskstrackermvvm/model/repository/TaskRepository.kt
+++ app/src/main/java/com/g2/taskstrackermvvm/model/repository/TaskRepository.kt
@@ -1,6 +1,14 @@
 package com.g2.taskstrackermvvm.model.repository
 
+import android.Manifest
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.CalendarContract
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.g2.taskstrackermvvm.model.Task
@@ -10,6 +18,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.net.URI
 import java.util.*
 
 interface ITaskRepo {
@@ -17,6 +26,7 @@ interface ITaskRepo {
     fun getListTask() : LiveData<List<Task>>
     fun setTag(taskId: String, tagId: String)
     fun removeTag(taskId: String, tagId: String)
+    fun exportCalendar(taskID: String)
 }
 
 class TaskRepositoryImp : ITaskRepo {
@@ -115,6 +125,51 @@ class TaskRepositoryImp : ITaskRepo {
             taskRef.child(user.uid).child(taskId).child(tagId).setValue(null)
             tagRef.child(user.uid).child(tagId).child(taskId).setValue(null)
         }
+    }
+
+    override fun exportCalendar(taskID: String) {
+        val calID:Long = 3
+        var startMillis:Long = 0
+        var endMillis:Long = 0
+        val beginTime:Calendar = Calendar.getInstance()
+        beginTime.set(2012, 9, 14, 7, 30)
+        startMillis = beginTime.timeInMillis
+        val endTime:Calendar = Calendar.getInstance()
+        endTime.set(2012, 9, 14, 8, 45)
+        endMillis = endTime.timeInMillis
+
+        //val context:Con
+        val cr:ContentResolver = ContentResolver()
+        val values:ContentValues = ContentValues()
+        values.put(CalendarContract.Events.DTSTART, startMillis)
+        values.put(CalendarContract.Events.DTEND, endMillis)
+        values.put(CalendarContract.Events.TITLE, "Jazzercise")
+        values.put(CalendarContract.Events.DESCRIPTION, "Group workout")
+        values.put(CalendarContract.Events.CALENDAR_ID, calID)
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, "America/Los_Angeles")
+        val uri: Uri? = if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_CALENDAR
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+            cr.insert(CalendarContract.Events.CONTENT_URI, values)
+
+// get the event ID that is the last element in the Uri
+        val eventID:Long = Long.parseLong(uri?.lastPathSegment)
+//
+// ... do something with event ID
+//
+//
+
     }
 
 
